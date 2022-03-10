1a91eb780e0f6714643a00447ba254d127d4d154
Eqbal78 <ahmadiqbal.a786@gmail.com>
2022-03-10 21:49:14 +0530

    [Eqbal]Added: Implemented Multiple Joins in the project

--- Practice.sql
+++ Practice.sql
@@ -269,3 +269,62 @@ select Customer_Name, Order_ID
 from Customers
 CROSS JOIN Order_Details
 where Customers.Customer_ID=Order_Details.Customer_ID
+
+----------------------------- Multiple Joins --------------------------------------
+
+create table Delivery
+(
+ Delivery_ID int identity (101,1) PRIMARY KEY,
+ Customer_ID int not null,
+ Delivery_Address varchar(255) not null,
+ Delivery_Status varchar(25) not null,
+)
+
+insert into Delivery(Customer_ID,Delivery_Address,Delivery_Status) values
+(2,'Mumbai','Delivered'),
+(5,'Mumbai','Delivered'),
+(10,'Patna','Not Delivered'),
+(1,'Pune','Delivered'),
+(3,'Delhi','Not Delivered'),
+(11,'Kolkata','Not Delivered'),
+(4,'Hyderabad','Delivered')
+
+select * from Delivery
+
+select Customer_Name, Customer_City, Delivery_Status, Delivery_Address
+from Customers as c
+INNER JOIN
+Order_Details as o
+on c.Customer_ID = o.Customer_ID
+INNER JOIN
+Delivery as d
+on o.Customer_ID = d.Customer_ID
+
+select Customer_Name, Delivery_Status, Delivery_Address
+from Customers as c
+INNER JOIN
+Order_Details as o
+on c.Customer_ID = o.Customer_ID
+INNER JOIN
+Delivery as d
+on o.Customer_ID = d.Customer_ID 
+where c.Customer_City = 'Mumbai'
+
+select c.Customer_ID , c.Customer_Name, Delivery_Status, Delivery_Address
+from Customers as c
+INNER JOIN
+Order_Details as o
+on c.Customer_ID = o.Customer_ID
+INNER JOIN
+Delivery as d
+on o.Customer_ID = d.Customer_ID 
+where c.Customer_ID between 2 and 5 order by c.Customer_ID
+
+select Customer_Name, Customer_Number, Order_Date, Delivery_Status
+from Customers as c
+INNER JOIN
+Order_Details as o
+on c.Customer_ID = o.Customer_ID
+RIGHT JOIN
+Delivery as d
+on o.Customer_ID = d.Customer_ID
